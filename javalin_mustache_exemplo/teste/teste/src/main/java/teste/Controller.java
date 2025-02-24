6c58a7c2b6d33a4a70e12854f0c99565ded2a6a3
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-02-24 18:05:41 -0300

    atualizando

--- javalin_mustache_exemplo/teste/teste/src/main/java/teste/Controller.java
+++ javalin_mustache_exemplo/teste/teste/src/main/java/teste/Controller.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Controller {
 
     public Map<String, ? extends Object> result() {        
-        Map<String, Object> t = new HashMap();
+        Map<String, Object> t = new HashMap<String, Object>();
         t.put("oi", "ola");
         return t;
     }
