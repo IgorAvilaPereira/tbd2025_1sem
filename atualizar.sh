1cd47e80139179cdb2e51497e8d4be4a2d271376
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-03-20 09:14:55 -0300

    atualizando

--- atualizar.sh
+++ atualizar.sh
@@ -7,7 +7,7 @@ git push
 
 repositorio=${PWD##*/}
 echo "# $repositorio <br>" > "README.md"
-echo "# $repositorio <br>" > "./../"$repositorio".wiki/Home.md"
+#echo "# $repositorio <br>" > "./../"$repositorio".wiki/Home.md"
 
 # listando tudo interno ao repositorio
 for d in ./*; do
@@ -15,7 +15,7 @@ for d in ./*; do
 	if [[ "$d" != "./README.md" && "$d" != "./atualizar.sh" && "$d" != "./nova_aula.sh" ]]; then
 		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) <br>" >> "README.md"		
 		# se n tiver wiki, favor comentar
-		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) <br>" >> "./../"$repositorio".wiki/Home.md"		
+#		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) <br>" >> "./../"$repositorio".wiki/Home.md"		
 		cd $d							
 
 		# documentos do subdiretorio de cada aula
@@ -23,10 +23,10 @@ for d in ./*; do
 		    if [[ "$d2" != "README.md" ]]; then		
 	    		echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) <br>" >> "./../README.md"		
 	    		# se n tiver wiki, favor comentar
-    			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) <br>" >> "./../../"$repositorio".wiki/Home.md"					
+ #   			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) <br>" >> "./../../"$repositorio".wiki/Home.md"					
             else
                  cat "README.md" >> "./../README.md"
-                 cat "README.md" >> "./../../"$repositorio".wiki/Home.md"							
+  #               cat "README.md" >> "./../../"$repositorio".wiki/Home.md"							
 			fi
 		done
 
@@ -35,17 +35,20 @@ for d in ./*; do
 			if [[ -d "$i" && ! -L "$i" ]]; then
 				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) <br>" >> "./../README.md"		
 				# se n tiver wiki, favor comentar
-				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) <br>" >> "./../../"$repositorio".wiki/Home.md"	
+	#			echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) <br>" >> "./../../"$repositorio".wiki/Home.md"	
 			fi
 		done
 		cd ..
 	fi
 done
 
+cp "README.md" "./../"$repositorio".wiki/Home.md"	
 git add * && git commit -m 'atualizando' && git push
+
 # se n tiver wiki, favor comentar
 cd "./../"$repositorio".wiki/" && git add * && git commit -m 'atualizando' && git push
+
 # exibindo o novo README criado. Este mesmo README aparecera no Home.md da wiki (caso exista)
-echo "==================================="
-cd "./../"$repositorio"/" && cat "README.md"	
-echo "==================================="
+#echo "==================================="
+#cd "./../"$repositorio"/" && cat "README.md"	
+#echo "==================================="
